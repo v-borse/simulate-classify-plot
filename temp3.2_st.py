fdd4a82aacf18bd990a7ca7726304273fe0ed529
v-borse <78208521+v-borse@users.noreply.github.com>
2021-02-02 11:15:58 +0100

    _1classify_lorenz63.py

--- temp3.2_st.py
+++ temp3.2_st.py
@@ -27,10 +27,8 @@ def classify2(_pt,q):
     _pt: takes vector (set of coordinates lying on trajectories) 
     q: the center of spherical region
     Returns:
-    ts: time series
-    # Soulivanh: It is probably not necessary to return the _c and _r variables as it is easy to deduce them from ts with the command np.where
-    _c0,_c1,_c2: number of points in region 0,1,2
-    _r0,_r1,_r2: Indices of the points in region 0,1,2
+    _ts: time series
+    
     
     dst: distance between two points
     R: radius of the sphere
@@ -41,8 +39,7 @@ def classify2(_pt,q):
     _r0=[]
     _r1=[]
     _r2=[]
-    # Soulivanh: if you know the final length of the vector, you should preallocate it. Here:
-    # _ts = np.zeros(_pn.shape[0])
+   
     _ts=[]
     R=1
     for each_ind, each_line in enumerate(_pt):
@@ -68,32 +65,21 @@ def classify2(_pt,q):
             _c0+=1
             # region 0: dst>R and x<=0
     
-    return _ts,_c0,_c1,_c2,_r0,_r1,_r2
+    return _ts
 
 #-----------------------------------------------------------------------------------------------------------------
 
-#arr_x=np.load('x.npy')
-#arr_y=np.load('y.npy')
-#arr_z=np.load('z.npy')
-#
-## creating a vector
-#pts=np.transpose(np.array([arr_x,arr_y,arr_z]))
+
 pts=np.load('pts.npy')
 q=np.array([15.0,13.0,37.0])
-# Soulivanh: q was not saved
+
 with open('q.npy', 'wb') as f:
         np.save(f, q)   
 
 
 # classifying regions
-ts,c0,c1,c2,R0,R1,R2=classify2(pts,q)
+ts=classify2(pts,q)
 
-# Soulivanh: It is simpler to save ts inshea as it is easy to deduce them from ts with the command np.where
+
 with open('ts.npy', 'wb') as g:
     np.save(g, ts)
-#with open('R2.npy', 'wb') as h:
-#        np.save(h, R2)   
-#with open('R0.npy', 'wb') as m:
-#        np.save(m, R0)    
-#with open('R1.npy', 'wb') as n:
-#        np.save(n, R1) 
