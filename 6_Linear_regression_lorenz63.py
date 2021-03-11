5534bfe7a319c23c82d862bb08b6f6216d2db08d
v-borse <78208521+v-borse@users.noreply.github.com>
2021-03-11 10:13:39 +0100

    Update 6_Linear_regression_lorenz63.py

--- 6_Linear_regression_lorenz63.py
+++ 6_Linear_regression_lorenz63.py
@@ -23,46 +23,9 @@ from sklearn.linear_model import LinearRegression
 from sklearn.preprocessing import PolynomialFeatures
 import matplotlib.pyplot as plt
 
+from module import lorenz
+from module import single_traj
 
-def single_traj(x0,y0,z0,r,dt,num_steps):
-    
-    pt=[]
-    #for single trajectory
-    xs = np.empty(num_steps + 1)
-    ys = np.empty(num_steps + 1)
-    zs = np.empty(num_steps + 1)
-        
-    # Set initial values
-    xs[0], ys[0], zs[0]= (4, -14, 21)
-    #xs[0], ys[0], zs[0] = (1., -1., 2.05)
-        
-        
-    for i in range(num_steps):
-      """
-      EULER SCHEME
-            
-      dt*x_dot, dt*y_dot, dt*z_dot = lorenz(xs[i], ys[i], zs[i])
-      xs[i + 1] = xs[i] + dt*x_dot
-      ys[i + 1] = ys[i] + dt*y_dot
-      zs[i + 1] = zs[i] + dt*z_dot
-      """
-      # RK 4 SCHEME
-            
-      
-            
-      k1_x, k1_y, k1_z = lorenz(xs[i], ys[i], zs[i], dt,r)
-      k2_x, k2_y, k2_z = lorenz(xs[i]+0.5*k1_x, ys[i]+0.5*k1_y, zs[i]+0.5*k1_z, dt,r)
-      k3_x, k3_y, k3_z = lorenz(xs[i]+0.5*k2_x, ys[i]+0.5*k2_y, zs[i]+0.5*k2_z, dt,r)
-      k4_x, k4_y, k4_z = lorenz(xs[i]+k3_x, ys[i]+k3_y, zs[i]+k3_z, dt,r)
-      xs[i + 1] = xs[i] + ((k1_x+2*k2_x+2*k3_x+k4_x) /6.0)
-      ys[i + 1] = ys[i] + ((k1_y+2*k2_y+2*k3_y+k4_y) /6.0)
-      zs[i + 1] = zs[i] + ((k1_z+2*k2_z+2*k3_z+k4_z) /6.0)
-    
-        
-    
-    pt=np.transpose(np.array([xs,ys,zs]))
-    
-    return pt
 
 
 #------MAIN BODY of the code -----------------------------------------------------------------------------------------------------------
