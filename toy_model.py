8fd1e78f81aa4b5d703188d4b7a749121c30dfff
Vaishnavi Borse <vborse@erakis007.home>
2021-04-23 17:47:07 +0200

    resolved generalized ideal fn

--- toy_model.py
+++ toy_model.py
@@ -22,33 +22,34 @@ f = np.arange(1,N+1,1)
 X=np.repeat(f,P).reshape(N,P)
 
 # STEP 2: CREATING POLYNOMIAL COVARIATES
-def Ideal_poly(X,order):
-    for i in range(1,order+1):
-        x=np.power(X,i)
+def Ideal_poly(_X,_order):
+    for i in range(1,_order+1):
+        x=np.power(_X,i)
         xx.append(x)
-        
-    return xx
+    XX=np.transpose(xx,(1,0,2)).reshape(N,-1)
+    
+    return XX
 
 
 
-def Ideal_lags(X,t_steps,t_lags):
+def Ideal_lags(_X,tsteps,tlags):
     Xti=[]
-    for i in range(0,t_lags+1):
-        xti=X[i:-(t_steps+t_lags-i)]
+    for i in range(0,tlags+1):
+        xti=X[i:-(tsteps+tlags-i)]
         Xti.append(xti)
         print(np.shape(xti))
-        
-    Y  =  X[(t_steps+t_lags):]
-    return(Xti,Y)
+    
+    l= np.size(Xti,axis=1)
+    Xx=np.transpose(Xti,(1,0,2)).reshape(l,-1)   
+    Y  =  X[(tsteps+tlags):]
+    
+    return(Xx,Y)
         
 Xt,Yt=Ideal_lags(X,t_steps,t_lags)
-l= np.size(Xt,axis=1)
-Xx=np.transpose(Xt,(1,0,2)).reshape(l,-1)    
 
-Xsss=Ideal_poly(X,order)
-XX=np.transpose(Xsss,(1,0,2)).reshape(N,-1)
+Xs=Ideal_poly(X,order)
 
 #print(Xx)
 #print(Yt)
-#
+
 #print(XX)
