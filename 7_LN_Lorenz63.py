60fa59083bda081b86128da444bfcf8fd1d5f380
v-borse <78208521+v-borse@users.noreply.github.com>
2021-06-08 14:50:13 +0200

    Update 7_LN_Lorenz63.py
    Gen Linear regression (OLS ie. without regularization)

--- 7_LN_Lorenz63.py
+++ 7_LN_Lorenz63.py
@@ -29,176 +29,12 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 import numpy as np
 
-from module2 import grouped_col_multi,grouped_col_uni2
-#from module import lorenz
-#from module import single_traj
-#
-#from toy_model import Ideal_poly
-#from toy_model import grouped_col_multi,swap, swap3,grouped_col_uni2,Ideal_poly3,Ideal_poly4
+import module
+import module2
 
 
-def plot_ts(Ytrue,ynrx,ynry,ynrz,Yr,YNRX,YNRY,YNRZ,YR):
-    
-    fig, axs = plt.subplots(6, 2, sharex=False, sharey=False, figsize=(15, 15))
-    fig.suptitle(' Linear Regression; dt=0.01')
-    
-    axs[0,0].plot(Ytrue[:,0],'b')
-    axs[0,0].plot(ynrx,'r')
-    #axs[0,0].plot(Ytrue[:,0]-ynrx,'g')
-    axs[0,0].set_xlabel("timesteps")
-    axs[0,0].set_ylabel("blue:Ytrue ; red: pred" )
-    axs[0,0].set_title("X component NR /Univariate")
-    
-    axs[1,0].plot(Ytrue[:,1],'b')
-    axs[1,0].plot(ynry,'r')
-    axs[1,0].set_xlabel("timesteps")
-    axs[1,0].set_ylabel("blue:Ytrue ; red: pred" )
-    axs[1,0].set_title("Y component NR /Univariate")
-    
-    axs[2,0].plot(Ytrue[:,2],'b')
-    axs[2,0].plot(ynrz,'r')
-    axs[2,0].set_xlabel("timesteps")
-    axs[2,0].set_ylabel("blue:Ytrue ; red: pred" )
-    axs[2,0].set_title("Z component NR /Univariate")
-    
-    axs[0,1].plot(Ytrue[:,0],'b')
-    axs[0,1].plot(YNRX,'r')
-    axs[0,1].set_xlabel("timesteps")
-    axs[0,1].set_ylabel("blue:Ytrue ; red: pred" )
-    axs[0,1].set_title("X component NR /Multivariate")
-    
-    axs[1,1].plot(Ytrue[:,1],'b')
-    axs[1,1].plot(YNRY,'r')
-    axs[1,1].set_xlabel("timesteps")
-    axs[1,1].set_ylabel("blue:Ytrue ; red: pred" )
-    axs[1,1].set_title("Y component NR /Multivariate")
-    
-    axs[2,1].plot(Ytrue[:,2],'b')
-    axs[2,1].plot(YNRZ,'r')
-    axs[2,1].set_xlabel("timesteps")
-    axs[2,1].set_ylabel("blue:Ytrue ; red: pred" )
-    axs[2,1].set_title("Z component NR /Multivariate")
-    
-    axs[3,0].plot(Ytrue[:,0])
-    axs[3,0].plot(Yp[:,0])
-    axs[3,0].set_xlabel("timesteps")
-    axs[3,0].set_ylabel("blue:Ytrue ; orange: pred" )
-    axs[3,0].set_title("X component R /Univariate")
-    
-    axs[4,0].plot(Ytrue[:,1])
-    axs[4,0].plot(Yp[:,1])
-    axs[4,0].set_xlabel("timesteps")
-    axs[4,0].set_ylabel("blue:Ytrue ; orange: pred" )
-    axs[4,0].set_title("Y component R /Univariate")
-    
-    axs[5,0].plot(Ytrue[:,2])
-    axs[5,0].plot(Yp[:,2])
-    axs[5,0].set_xlabel("timesteps")
-    axs[5,0].set_ylabel("blue:Ytrue ; orange: pred" )
-    axs[5,0].set_title("Z component R /Univariate")
-    
-    axs[3,1].plot(Ytrue[:,0])
-    axs[3,1].plot(YP[:,0])
-    axs[3,1].set_xlabel("timesteps")
-    axs[3,1].set_ylabel("blue:Ytrue ; orange: pred" )
-    axs[3,1].set_title("X component R /Multivariate")
-    
-    axs[4,1].plot(Ytrue[:,1])
-    axs[4,1].plot(YP[:,1])
-    axs[4,1].set_xlabel("timesteps")
-    axs[4,1].set_ylabel("blue:Ytrue ; orange: pred" )
-    axs[4,1].set_title("Y component R /Multivariate")
-    
-    axs[5,1].plot(Ytrue[:,2])
-    axs[5,1].plot(YP[:,2])
-    axs[5,1].set_xlabel("timesteps")
-    axs[5,1].set_ylabel("blue:Ytrue ; orange: pred" )
-    axs[5,1].set_title("Z component R /Multivariate")
-    
-    fig.tight_layout()
-    
-    
-def Ideal_lags(_X,tsteps,tlags):
-    Xti=_X[:-(tsteps+tlags)]
-    for i in range(1,tlags+1):
-        
-        Xti=np.concatenate((Xti, _X[i:-(tsteps+tlags-i)]), axis=1)
-            
-    Y  =_X[(tsteps+tlags):]
-    
-    return(Xti,Y)   
-    
-def swap_multi(_X,_Y,modelx,modely,modelz,t_steps,t_lags,ncol,order):
-    
-      
-    _X = np.copy(_X)
-    
-    c=grouped_col_multi(ncol,t_lags,order)
 
-    for i in range(1,t_steps+1):
-               
-        Ynrx = modelx.predict(_X)
-        Ynry = modely.predict(_X)
-        Ynrz = modelz.predict(_X)
-        
-        Xnew=np.array([Ynrx,Ynry,Ynrz])
-               
-        for k in range(t_lags+1):
-          
-            
-            if ((k+1)<=t_lags):
-                
-                _X.T[:,[c[k]]] = _X.T[:,[c[k+1]]]
-               
-            else:
-                
-                xx=Ideal_poly3(Xnew.T,order,t_steps)              
-                _X[:,c[-1]] = xx
-       
-    return _X[:,c[-1]]
 
-def swap_uni(_X,_Y,modelx,modely,modelz,t_steps,t_lags,ncol,order):
-    
-     
-    _X = np.copy(_X)
-    c=grouped_col_multi(ncol,t_lags,order)   
-    cu=grouped_col_uni2(ncol,t_lags,order)
-
-    for i in range(1,t_steps+1):
-        
-
-        YNrx = modelx.predict(_X[:,cu[0]])
-        
-        YNry = modely.predict(_X[:,cu[1]])
-        YNrz = modelz.predict(_X[:,cu[2]])
-        
-       
-        Xnew=np.array([YNrx,YNry,YNrz])
-        
-        for k in range(t_lags+1):
-            
-            
-            if ((k+1)<=t_lags):
-                
-                _X.T[:,[c[k]]] = _X.T[:,[c[k+1]]]
-                
-            else:
-                
-#                A=Xnew.T
-#                #print(np.shape(A))                
-#                B=np.transpose(A,(1,0,2)).reshape(3,-1)
-#                #print(np.shape(B))
-#                xx=Ideal_poly3(B.T,order,t_steps)
-                A=np.squeeze(Xnew, axis=2)
-                xx=Ideal_poly3(A.T,order,t_steps)
-                
-                
-                #print(np.shape(_X[:,c[-1]]))
-                
-                _X[:,c[-1]] = xx
-        
-    return _X[:,c[-1]]
-    
 #----FORMING LORENZ TRAJECTORIES-------
 
 r=28
@@ -251,13 +87,13 @@ Ynrz=regr_z.predict(Xtest[:,cu[2]])
 #----RECURSIVE-------------------------------------
 
 rx=linear_model.LinearRegression()
-rx.fit(Xr_train[:,cu[0]], Yr_train[:,0,None])
+rx.fit(Xr_train[:,cu[0]], Yr_train[:,0,])
 
 ry=linear_model.LinearRegression()
-ry.fit(Xr_train[:,cu[1]], Yr_train[:,1,None])
+ry.fit(Xr_train[:,cu[1]], Yr_train[:,1])
 
 rz=linear_model.LinearRegression()
-rz.fit(Xr_train[:,cu[2]], Yr_train[:,2,None])
+rz.fit(Xr_train[:,cu[2]], Yr_train[:,2])
 
 Yp=swap_uni(Xtest,Ytest,rx,ry,rz,t_steps,t_lags,ncol,order)
 
